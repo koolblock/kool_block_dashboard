6f1fd1ffb10846f08ee0c26469bdb116fcfa427f
Eduardo Landa <eduardo.landa@outlook.com>
2021-03-17 17:44:46 -0700

    Remove .next folder

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,58 +3,58 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer class="footer-distributed">
-      <div class="footer-left">
+    <footer className="footer-distributed">
+      <div className="footer-left">
         <h3>
           Kool<span>Block</span>
         </h3>
 
-        <p class="footer-links">
-          <Link href="/" class="link-1">
+        <p className="footer-links">
+          <Link href="/" className="link-1">
             Home
           </Link>
         </p>
 
-        <p class="footer-company-name">KoolBlock © 2021</p>
+        <p className="footer-company-name">KoolBlock © 2021</p>
       </div>
 
-      <div class="footer-center">
+      <div className="footer-center">
         <div>
-          <i class="fa fa-map-marker"></i>
+          <i className="fa fa-map-marker"></i>
           <p>
             <span>777 Lucky Seven, Casino</span> Lucky City, Lucky Province
           </p>
         </div>
 
         <div>
-          <i class="fa fa-phone"></i>
+          <i className="fa fa-phone"></i>
           <p>+1.777.777.7777</p>
         </div>
 
         <div>
-          <i class="fa fa-envelope"></i>
+          <i className="fa fa-envelope"></i>
           <p>
             <Link href="/contact_us">Contact Us</Link>
           </p>
         </div>
       </div>
 
-      <div class="footer-right">
-        <p class="footer-company-about">
+      <div className="footer-right">
+        <p className="footer-company-about">
           <span>About the company</span>
           Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce
           euismod convallis velit, eu auctor lacus vehicula sit amet.
         </p>
 
-        <div class="footer-icons">
+        <div className="footer-icons">
           <a href="#">
-            <i class="fa fa-facebook"></i>
+            <i className="fa fa-facebook"></i>
           </a>
           <a href="#">
-            <i class="fa fa-twitter"></i>
+            <i className="fa fa-twitter"></i>
           </a>
           <a href="#">
-            <i class="fa fa-linkedin"></i>
+            <i className="fa fa-linkedin"></i>
           </a>
         </div>
       </div>
--- app/pages/blog/[id].js
+++ app/pages/blog/[id].js
@@ -16,12 +16,45 @@ export const GET_POST = gql`
   }
 `;
 
+export const GET_ALL_POSTS_ID = gql`
+  query {
+    allPosts {
+      id
+    }
+  }
+`;
+
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const { loading, error, data } = useQuery(GET_ALL_POSTS_ID);
+  const paths = data.allPosts.map((post) => ({
+    params: { id: post.id },
+  }));
+  console.log(paths);
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
+
+// // This also gets called at build time
+// export async function getStaticProps({ params }) {
+//   // params contains the post `id`.
+//   // If the route is like /posts/1, then params.id is 1
+//   const res = await fetch(`https://.../posts/${params.id}`)
+//   const post = await res.json()
+
+//   // Pass post data to the page via props
+//   return { props: { post } }
+// }
+
 export default function Index() {
-  const router = useRouter();
-  const id = router?.query?.id;
-  const { loading, error, data } = useQuery(GET_POST, {
-    variables: { id },
-  });
+  // const router = useRouter();
+  // const id= router?.query?.id;
+  // const { loading, error, data } = useQuery(GET_POST, {
+  //   variables: { id },
+  // });
+  // console.log(data);
 
   return <div>I EXIST</div>;
 }
